e1b073d4958dc8945c4bb9acfa2b300637b14fe9
Ruben-JR <rubenpina758@gmail.com>
2022-03-28 08:06:08 -0100

    First commit

---
+++ Grupo_RubenPina_9414_IvaldirBatalha_9485_HumbertoPuga_11224_Projeto_Computacional_AL_2021_2022.py
@@ -0,0 +1,120 @@
+import numpy as np
+
+#-------------------------------------------------------------Exercicio_1_2
+#Exercicio 1
+#--------------Alinea A de 1
+def  transposta():
+    print()
+
+#--------------Alinea B de 1
+def  traco():
+    print()
+
+#--------------Alinea C de 1
+def traco_secundaria():
+    print()
+
+#--------------Alinea D de 1
+def soma_mat():
+    print()
+
+#--------------Alinea E de 1
+def mult_escalar_mat():
+    print()
+
+#--------------Alinea F de 1
+def prod_mat():
+    print()
+
+#Exercicio 2
+#--------------Testar as funcoes do exercico1 usando as matrizes do exercicio 2
+#--------------Funcao (a) de 1
+transposta()
+
+#--------------Funcao B de 1
+traco()
+
+#--------------Funcao C de 1
+traco_secundaria()
+
+#--------------Funcao D de 1
+soma_mat()
+
+#--------------Funcao E de 1
+mult_escalar_mat()
+
+#--------------Funcao F de 1
+prod_mat()    
+
+#------------------------------------------------------------Exercicio_3_4
+#Exercicio 3
+#--------------Algoritmo 1 de 3
+def forma_escalonada():
+    print()
+
+#--------------Algoritmo 2 de 3
+def forma_escalonada_reduzida_por_linha():
+    print()
+
+#--------------Alinea (a) de 3
+def primeira_col_nao_nula():
+    print()
+
+#--------------Alinea (b) de 3
+def troca_linhas():
+    print()
+
+#--------------Alinea (c) de 3
+def cria_zeros():
+    print()
+
+#--------------Alinea (d) de 3
+def elimina_linha_1():
+    print()
+
+#--------------Alinea (e) de 3
+def forma_escalonada():
+    print()
+
+#--------------Alinea (f) de 3
+def  forma_escalonada_reduzida():
+    print()
+
+#--------------Alinea (g) de 3
+def gauss_jordan():
+    print()
+
+#Exercicio 4
+#--------------Teste a funcao gauss_jordan usando as matrizes de 4, dando uma especial atenção à matriz B
+gauss_jordan()
+
+#-------------------------------------------------------------Exercicio_5_6
+#Exercicio 5
+# -------------Alinea (a) de 5
+def posto():
+    print()
+
+#--------------Alinea (b) de 5
+def sistema_eq_linear():
+    print()
+
+#Exercicio 6
+#--------------Testar  as funcoes de 5 
+# -------------Funcao (a) de 5
+posto()
+
+#--------------Funcao (b) de 5
+sistema_eq_linear()
+
+#-------------------------------------------------------------Exercicio_7_8
+#Exercicio 7
+def determinante():
+    print()
+    
+    forma_escalonada()
+    print()
+
+#Exercicio 8
+#--------------Testar a funcao de 7 usando as matrizes de 8
+#--------------Funcao de 7
+determinante()
---
+++ READMe.md
@@ -0,0 +1,28 @@
+<h3>Algebra project</h3> 
+
+<h3>About the project</h3>
+
+<p>The aim of this project is to put into practice the knowledge acquired in the subject Linear Algebra in my computer systems engineering course. The project also aims to practice the knowledge about matrices in Python. This is a group project done together with other colleagues.</p>   
+
+<h3>Objective</h3> 
+
+<p>The goal of this project is to create functions in Python to work with matrices, and make me and other colleagues betters developers.</p>
+
+<h3>Technology used</h3>
+
+    1 - VSCode
+    2 - Python
+
+ <h3>Author</h3>
+ 
+<p>Ruben de Pina</p>
+ <p>Linkedin: https://www.linkedin.com/in/ruben-pina-3851b4235/</p>
+ <p>email: rubenpina758@gmail.com</p>
+
+<p>Ivaldir Batalha</p>
+ <p>Linkedin: https://www.linkedin.com/in/ivaldir-batalha-a67952185/</p>
+ <p>GitHub: https://github.com/ivaldir301</p>
+
+<p>Humberto Puga</p>
+
+https://img.shields.io/badge/Status-In%20progress-yellow
